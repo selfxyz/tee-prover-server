26b6480a0eda49b3e34ee4e7bf18808378276035
ayman <aymanshaik1015@gmail.com>
2025-03-05 01:30:39 +0530

    feat: move 384 bit circuits into medium instances

--- circuit_eliminator.sh
+++ circuit_eliminator.sh
@@ -1,20 +1,54 @@
 #!/bin/bash
 
-register_circuits=("register_sha512_sha512_sha512_ecdsa_brainpoolP512r1" "register_sha384_sha384_sha384_ecdsa_brainpoolP512r1")
-dsc_circuits=("dsc_sha384_ecdsa_brainpoolP512r1" "dsc_sha512_ecdsa_brainpoolP512r1")
+register_circuits=(
+  "register_sha512_sha512_sha512_ecdsa_brainpoolP512r1:large" 
+  "register_sha384_sha384_sha384_ecdsa_brainpoolP512r1:large" 
+  "register_sha256_sha256_sha256_ecdsa_brainpoolP384r1:medium" 
+  "register_sha256_sha256_sha256_ecdsa_secp384r1:medium" 
+  "register_sha384_sha384_sha384_ecdsa_brainpoolP384r1:medium" 
+  "register_sha384_sha384_sha384_ecdsa_secp384r1:medium"
+)
+dsc_circuits=(
+  "dsc_sha384_ecdsa_brainpoolP512r1:large" 
+  "dsc_sha512_ecdsa_brainpoolP512r1:large" 
+  "dsc_sha256_ecdsa_brainpoolP384r1:medium" 
+  "dsc_sha256_ecdsa_secp384r1:medium" 
+  "dsc_sha384_ecdsa_brainpoolP384r1:medium" 
+  "dsc_sha384_ecdsa_secp384r1:medium"
+)
 disclose_circuits=()
 
 proof_type=$1
+size_filter=$2
+
+echo $proof_type
+echo $size_filter
+declare -A keep_circuits
+
+define_circuits() {
+  local size_filter="$1"
+  local -n circuits_ref=$2
+  for circuit in "${circuits_ref[@]}"; do
+    IFS=":" read -r name size <<< "$circuit"
+    if [[ "$size_filter" == "small" ]]; then
+      keep_circuits["/circuits/${name}_cpp"]=-1
+      keep_circuits["/zkeys/${name}.zkey"]=-1
+    elif [[ "$size_filter" == "$size" ]]; then
+      keep_circuits["/circuits/${name}_cpp"]=1
+      keep_circuits["/zkeys/${name}.zkey"]=1
+    fi
+  done
+}
 
 case "$proof_type" in
   "register")
-    circuits=("${register_circuits[@]}")
+    define_circuits "$size_filter" register_circuits
     ;;
   "dsc")
-    circuits=("${dsc_circuits[@]}")
+    define_circuits "$size_filter" dsc_circuits
     ;;
   "disclose")
-    circuits=("${disclose_circuits[@]}")
+    define_circuits "$size_filter" disclose_circuits
     ;;
   *)
     echo "Invalid proof type: $proof_type"
@@ -22,10 +56,21 @@ case "$proof_type" in
     ;;
 esac
 
-for circuit in "${circuits[@]}"; do
-  echo /circuits/${circuit}_cpp
-  rm -rf "/circuits/${circuit}_cpp"
-  rm -rf "/zkeys/${circuit}.zkey"
+all_circuit_dirs=(/circuits/*_cpp)
+all_zkey_files=(/zkeys/*.zkey)
+
+for dir in "${all_circuit_dirs[@]}"; do
+  if [[ ("$size_filter" != "small" && ! -v keep_circuits[$dir]) || keep_circuits[$dir] -eq -1 ]]; then
+    echo "Removing $dir"
+    rm -rf "$dir"
+  fi
 done
 
-echo "Cleanup complete for $proof_type circuits."
+for file in "${all_zkey_files[@]}"; do
+  if [[ ("$size_filter" != "small" && ! -v keep_circuits[$file]) || keep_circuits[$file] -eq -1 ]]; then
+    echo "Removing $file"
+    rm -rf "$file"
+  fi
+done
+
+echo "Cleanup complete, only specified $proof_type circuits of size $size_filter remain."
